b849f179c571b6dfa013b8a9d7e3a37c25f08eb8
Ibrahim <edinstvenniy123@gmail.com>
2023-04-12 11:54:39 +0300

    Add implementation for CommonDAO

--- src/main/java/ru/msu/cmc/webprak/DAO/impl/CommonDAOImpl.java
+++ src/main/java/ru/msu/cmc/webprak/DAO/impl/CommonDAOImpl.java
@@ -1,7 +1,6 @@
 package ru.msu.cmc.webprak.DAO.impl;
 
 
-import jakarta.persistence.Id;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 
@@ -9,7 +8,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
 
 import ru.msu.cmc.webprak.DAO.CommonDAO;
