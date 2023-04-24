a8abae25ebdf1bc596037129b7986148b5f5604f
Ibrahim <edinstvenniy123@gmail.com>
2023-04-25 00:44:51 +0300

    Fix WebrapApplication code style

--- src/main/java/ru/msu/cmc/webprak/WebprakApplication.java
+++ src/main/java/ru/msu/cmc/webprak/WebprakApplication.java
@@ -1,7 +1,9 @@
 package ru.msu.cmc.webprak;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class WebprakApplication {
