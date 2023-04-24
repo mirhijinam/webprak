3ca549e497baddd5733e0b41d2b29de883f4fbab
Ibrahim <edinstvenniy123@gmail.com>
2023-04-25 00:52:10 +0300

    Fix WebprakApplicationTests code style

--- src/test/java/ru/msu/cmc/webprak/WebprakApplicationTests.java
+++ src/test/java/ru/msu/cmc/webprak/WebprakApplicationTests.java
@@ -1,7 +1,9 @@
 package ru.msu.cmc.webprak;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class WebprakApplicationTests {
